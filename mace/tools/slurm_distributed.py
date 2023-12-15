b211a25f3ef5c4db973a6fd3cc333b767abf3f09
khegazy <KareemHegz@gmail.com>
2023-12-14 17:16:06 -0800

    Can now run locally on an interactive node with multiple gpus.

--- mace/tools/slurm_distributed.py
+++ mace/tools/slurm_distributed.py
@@ -11,12 +11,20 @@ import hostlist
 
 class DistributedEnvironment():
     def __init__(self):
-        self._setup_distr_env()
-        self.master_addr = os.environ['MASTER_ADDR']
-        self.master_port = os.environ['MASTER_PORT']
+        if 'SLURM_NTASKS' in os.environ.keys():
+            self._setup_distr_env()
+        if 'MASTER_ADDR' in os.environ.keys():
+            self.master_addr = os.environ['MASTER_ADDR']
+            self.master_port = os.environ['MASTER_PORT']
         self.world_size = int(os.environ['WORLD_SIZE'])
         self.local_rank = int(os.environ['LOCAL_RANK'])
         self.rank = int(os.environ['RANK'])
+        print("SETUP DISTRIBUTED")
+        print("rank", self.rank)
+        print("local rank", self.local_rank)
+        print("WS", self.world_size)
+        print("MA", self.master_addr)
+        print("MP", self.master_port)
 
     def _setup_distr_env(self):
         hostname = hostlist.expand_hostlist(os.environ['SLURM_JOB_NODELIST'])[0]
