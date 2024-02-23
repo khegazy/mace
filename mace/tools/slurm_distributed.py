41c80aa95573c3ffb9ff3a1acd9befd4ec8e7dd3
khegazy <KareemHegz@gmail.com>
2024-02-23 13:12:26 -0800

    Fixing SLURM parallal training issue with MASTER_PORT.

--- mace/tools/slurm_distributed.py
+++ mace/tools/slurm_distributed.py
@@ -29,7 +29,7 @@ class DistributedEnvironment():
     def _setup_distr_env(self):
         hostname = hostlist.expand_hostlist(os.environ['SLURM_JOB_NODELIST'])[0]
         os.environ['MASTER_ADDR'] = hostname
-        os.environ['MASTER_PORT'] = os.environ.get('MASTER_PORT', '33333')
+        os.environ['MASTER_PORT'] = '33333'#os.environ.get('MASTER_PORT', '33333')
         os.environ['WORLD_SIZE'] = os.environ['SLURM_NTASKS']
         os.environ['LOCAL_RANK'] = os.environ['SLURM_LOCALID']
         os.environ['RANK'] = os.environ['SLURM_PROCID']
